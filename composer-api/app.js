0fff569c6df3215a2a63afa13f957d9527f12f74
Ben Hilarides <ben.hilarides26@gmail.com>
2026-01-24 17:05:26 -0500

    Week2-Minor: Change Submit to Get Data

---
+++ composer-api/app.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+//var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/composers', indexRouter);
+//app.use('/users', usersRouter);
+
+app.use((req, res, next) => {
+    next(createError(404));
+})
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+
+    res.json({
+        type: 'error',
+        status: res.status,
+        message: err.message,
+        stack: req.app.get('env') === 'development'?err.stack: undefined
+    })
+});
+
+module.exports = app;
